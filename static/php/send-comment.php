a3f73e41714564717ff6f24e0d62a09f85e1dc87
Maurizio Paglia <mpaglia0@gmail.com>
2021-03-10 13:07:22 +0100

    Update send-comment.php

--- static/php/send-comment.php
+++ static/php/send-comment.php
@@ -6,7 +6,7 @@ if(empty($_POST['name'])      ||
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-   echo "Non hai completato tutti i campi!";
+   echo "Please fill all required fields!";
    return false;
    }
    
@@ -17,8 +17,8 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
 $to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Hai un nuovo messaggio da $name";
-$email_body = "Hai ricevuto un nuovo messaggio dal tuo sito web.\n\n"."Ecco i dettagli:\n\nNome: $name\n\nEmail: $email_address\n\nWebsite: $website\n\nMessaggio:\n$message";
+$email_subject = "New comment from $name";
+$email_body = "You received a new comment on your website.\n\n"."All details are here below:\n\nName: $name\n\nEmail: $email_address\n\nWebsite: $website\n\nComment:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
