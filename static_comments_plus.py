6038ef109a758fbafb7faea8c83db7b9610b5bcb
Maurizio Paglia <mpaglia0@gmail.com>
2021-04-17 12:36:38 +0200

    Add files via upload
    Uploaded the first stable version

--- static_comments_plus.py
+++ static_comments_plus.py
@@ -1,54 +1,65 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
 import codecs
 import logging
+import markdown
 import os
 
 logger = logging.getLogger(__name__)
 
 from pelican import signals
+from pelican.readers import RstReader
 
 
 def initialized(pelican):
     from pelican.settings import DEFAULT_CONFIG
-    DEFAULT_CONFIG.setdefault('STATIC_COMMENTS_PLS', False)
+    DEFAULT_CONFIG.setdefault('STATIC_COMMENTS_PLUS', False)
     DEFAULT_CONFIG.setdefault('STATIC_COMMENTS_DIR' 'comments')
-    DEFAULT_CONFIG.setdefault('STATIC_COMMENTS_EXT' '.rst')
     if pelican:
-        pelican.settings.setdefault('STATIC_COMMENTS_PLS', False)
+        pelican.settings.setdefault('STATIC_COMMENTS_PLUS', False)
         pelican.settings.setdefault('STATIC_COMMENTS_DIR', 'comments')
-        pelican.settings.setdefault('STATIC_COMMENTS_EXT', '.rst')       
+
 
 def add_static_comments(gen, metadata):
-  
-    if gen.settings['STATIC_COMMENTS_PLS'] != True:
-        logger.warning("static_comments_plus: "
-                "plugin is installed but NOT activated...")
+    if gen.settings['STATIC_COMMENTS_PLUS'] != True:
+        logger.info("Static_Comments_Plus: plugin is not enabled...")
         return
 
     if not 'slug' in metadata:
         logger.warning("Static_Comments_Plus: "
-                "cant't locate slug tag in the article...")
-        return
-
-    fname = os.path.join(gen.settings['STATIC_COMMENTS_DIR'],metadata['slug'],gen.settings['STATIC_COMMENTS_EXT'])
-
-    if not os.path.exists(fname):
-        logger.warning("static_comments_plus: "
-                "cant't locate comments path! Please check your configuration parameters...")
-        return
-
-    if gen.settings['STATIC_COMMENTS_EXT'] == ".md":
-        import markdown
-    else
-        from docutils import nodes
-        from docutils.parsers.rst import directives, Directive
+                "cant't locate comments file without slug tag in the article!")
+        
+    no_comment_msg = "Static_Comments_Plus: NO comments file for ... " + metadata['slug']
     
-    input_file = codecs.open(fname, mode="r", encoding="utf-8")
-    text = input_file.read()
-    html = markdown.markdown(text)
+    parse_mode = gen.settings.get('STATIC_COMMENTS_SOURCE', 'MD')
+    if parse_mode not in ['RST', 'MD']:
+        logger.warning("Static_Comments_Plus: "
+                "Invalid option {} as the plugin option... "
+                "STATIC_COMMENTS_SOURCE can only be set to MD or RST. Defaulting to MD".format(parse_mode))
+    if parse_mode == 'RST':
+        logger.info('Static_Comments_Plus: comment parser is set to RST')
+        fname = os.path.join(gen.settings['STATIC_COMMENTS_DIR'], metadata['slug'] + ".rst")
+        if not os.path.exists(fname):
+            logger.info(no_comment_msg)
+            return
+        reader = RstReader(settings=gen.settings)
+        html, _ = reader.read(source_path=fname)
+        logger.info("Static_Comments_Plus: RST comment file opened")
+
+    else:  # the default parser is Markdown parser, hence 'MD'
+        fname = os.path.join(gen.settings['STATIC_COMMENTS_DIR'], metadata['slug'] + ".md")
+        logger.info('Static_Comments_Plus: comment parser is set to MD')
+        if not os.path.exists(fname):
+            logger.info(no_comment_msg)
+            return
+        input_file = codecs.open(fname, mode="r", encoding="utf-8")
+        logger.info("Static_Comments_Plus: MD comment file opened")
+        text = input_file.read()
+        html = markdown.markdown(text)
+
+    # logger.info(html)
     metadata['static_comments'] = html
+
 
 def register():
     signals.initialized.connect(initialized)
